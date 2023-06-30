8a0b6fe1854245376895864e605f3e12ad05ad5b
Pascal Mika <pascl.mika@t-online.de>
2023-06-30 11:25:26 +0200

    footer test

--- website/src/components/Footer.tsx
+++ website/src/components/Footer.tsx
@@ -1,34 +1,80 @@
-import React from 'react';
-import { Typography, Link, Grid, Container } from '@mui/material';
+import * as React from "react";
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
+import Grid from "@mui/material/Grid";
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
+import { Box } from "@mui/material";
 
-const footerStyles = {
-  padding: '16px',
-  marginTop: 'auto',
-  backgroundColor: '#f5f5f5',
-};
-
-const Footer = () => {
+export default function Footer() {
   return (
-    <footer style={footerStyles}>
-      <Container maxWidth="md">
-        <Grid >
-          <Grid item>
-            <Typography variant="body1">© 2023 Entchen Gaming</Typography>
+    <Box
+      component="footer"
+      sx={{
+        backgroundColor: "rgba(0, 0, 0, 0)", // Transparent background
+        p: 6,
+        display: "flex", // Center content horizontally
+        justifyContent: "center", // Center content horizontally
+      }}
+    >
+      <Container maxWidth="lg">
+        <Grid container spacing={1}>
+          <Grid item xs={18} sm={6}>
+            <Typography variant="h6" color="#ffff01" gutterBottom>
+              Über uns
+            </Typography>
+            <Typography variant="body2" color="#ffff01">
+              Hi, wir sind Entchen Gaming
+            </Typography>
           </Grid>
-          <Grid item>
-            <Link href="#" variant="body1">
-              Privacy Policy
+          <Grid item xs={18} sm={6}>
+            <Typography variant="h6" color="#ffff01" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/" color="#ffff01">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="#ffff01"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://www.twitter.com/" color="#ffff01">
+              <Twitter />
             </Link>
           </Grid>
-          <Grid item>
-            <Link href="#" variant="body1">
-              Terms of Service
+          <Grid item xs={18} sm={6}>
+            <Typography variant="h6" color="#ffff01" gutterBottom>
+              Follow Us
+            </Typography>
+            <Link href="https://www.facebook.com/" color="#ffff01">
+              <Facebook />
+            </Link>
+            <Link
+              href="https://www.instagram.com/"
+              color="#ffff01"
+              sx={{ pl: 1, pr: 1 }}
+            >
+              <Instagram />
+            </Link>
+            <Link href="https://www.twitter.com/" color="#ffff01">
+              <Twitter />
             </Link>
           </Grid>
         </Grid>
+        <Box mt={5}>
+          <Typography variant="body2" color="#ffff01" align="center">
+            {"Copyright "}
+            <Link color="#ffff01" href="https://your-website.com/">
+              Entchen Gaming
+            </Link>{" "}
+            {new Date().getFullYear()}
+            {"."}
+          </Typography>
+        </Box>
       </Container>
-    </footer>
+    </Box>
   );
-};
-
-export default Footer;
+}
